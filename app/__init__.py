52d8c76af6af0d803b6e2fbf744442193b96c23c
Akinyele Thompson <akinyele.thompson@mymona.uwi.edu>
2017-04-30 19:48:13 +0000

    heroku database config

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_triangle import Triangle
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://info3180:wishlist123@localhost/Wishlist_API"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://pzokgxmrhnihio:dc65deddbd1f4b0da50c8cf5e8384bcf0f46d11e2876168a4ce32308e358af03@ec2-54-225-119-223.compute-1.amazonaws.com:5432/d3fpge3uckt2o7"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://argwfcrcooizrs:08f022a4819b7993f8c278d3b84dfc3e5613d715b6e5a074dc2d66d948ed6670@ec2-174-129-41-23.compute-1.amazonaws.com:5432/d85lpejkfivj6r"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
